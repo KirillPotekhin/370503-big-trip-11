e60542dad6228cbdf7d6a53e2ad9f981dc527ec0
kirillpotekhin <kirill.potekhin@gmail.com>
2020-05-23 12:05:37 +0700

    рефакторинг типов

--- src/const.js
+++ src/const.js
@@ -1,14 +1,14 @@
 const TYPES = [
-  `Taxi`,
-  `Bus`,
-  `Train`,
-  `Ship`,
-  `Transport`,
-  `Drive`,
-  `Flight`,
-  `Check-in`,
-  `Sightseeing`,
-  `Restaurant`
+  `taxi`,
+  `bus`,
+  `train`,
+  `ship`,
+  `transport`,
+  `drive`,
+  `flight`,
+  `check-in`,
+  `sightseeing`,
+  `restaurant`
 ];
 
 const DAY = 24 * 60 * 60 * 1000;
